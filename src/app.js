2a4922ee5d014651e219f4718c2407a371ddd008
khalid2305 <get2khalid.b@gmail.com>
2025-11-23 18:25:01 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,38 @@
-console.log("Did it")
-
 const express=require('express')
 
 const app=express()
+// app.get("/route",[rh1,rh2],rh3)
 
-app.get("/user",function(req,res){
-    console.log(req.query)
-    res.send({firstname:"khalid",lastname:"B"})
+app.get(
+    "/route",
+    [(req,res,next)=>{
+        
+  console.log("rh1")
+    // res.send("from rh1")
+  next()
+//   res.send("from rh1")
+
+},(req,res,next)=>{
+
+  console.log("rh2")
+    // res.send("from rh2")
+   next()
+
+}],(req,res,next)=>{
+  console.log("rh3")
+  res.send("from rh3")
+next()
 })
 
-app.get("/user2/:userid/:name/:password",function(req,res){
-    console.log(req.params)
-    res.send({firstname:"khalid",lastname:"B"})
-})
+// app.get("/user",function(req,res){
+//     console.log(req.query)
+//     res.send({firstname:"khalid",lastname:"B"})
+// })
+
+// app.get("/user2/:userid/:name/:password",function(req,res){
+//     console.log(req.params)
+//     res.send({firstname:"khalid",lastname:"B"})
+// })
 
 app.post("/user",function(req,res){
     res.send("Successfully posted an api")
@@ -21,6 +41,7 @@ app.post("/user",function(req,res){
 app.delete("/user",function(req,res){
     res.send("Deleted successfully")
 })
+
 app.use("/Hello",function(req,res){
    res.send("Hi macchi")
 })

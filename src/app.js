a71c04008dec54059a51ad06f29d2ed4e981b5a5
khalid2305 <get2khalid.b@gmail.com>
2025-11-23 11:26:23 +0530

    exploring post,get,delete

--- src/app.js
+++ src/app.js
@@ -4,6 +4,18 @@ const express=require('express')
 
 const app=express()
 
+app.get("/user",function(req,res){
+    res.send({firstname:"khalid",lastname:"B"})
+})
+
+app.post("/user",function(req,res){
+    console.log(req.body)
+    res.send("Successfully posted an api")
+})
+
+app.delete("/user",function(req,res){
+    res.send("Deleted successfully")
+})
 app.use("/Hello",function(req,res){
    res.send("Hi macchi")
 })
